113784377c2f046e9b5ecb8be9faf7c23340ddd8
somecallmejosh <josh@thebrileys.com>
2013-12-09 23:02:45 -0500

    Update static page controllers to include stat calculations.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+  	@field_goals = FieldGoal.all
+  	@total_field_goals = FieldGoal.count(:conditions => { :is_good => true }) * 3
+  	@field_goal_long = FieldGoal.maximum('distance', :conditions => {:is_good => true})
+
+  	@kickoffs = Kickoff.all
+    @total_kickoff_yards = Kickoff.sum('distance')
+    @kickoff_long = Kickoff.maximum('distance')
+
   end
   def bio
   end
