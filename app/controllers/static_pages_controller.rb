be1c4db87f29b5043b9d2ad4cb89448699e79249
somecallmejosh <josh@thebrileys.com>
2013-12-09 23:16:53 -0500

    Reorganize home page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,17 @@ class StaticPagesController < ApplicationController
   	@total_field_goals = FieldGoal.count(:conditions => { :is_good => true }) * 3
   	@field_goal_long = FieldGoal.maximum('distance', :conditions => {:is_good => true})
 
+  	@point_after_attempts = PointAfterAttempt.all
+  	@total_pats = PointAfterAttempt.count(:conditions => { :is_good => true })
+
   	@kickoffs = Kickoff.all
     @total_kickoff_yards = Kickoff.sum('distance')
     @kickoff_long = Kickoff.maximum('distance')
+
+    @punts = Punt.all
+    @punt_long = Punt.maximum('distance')
+
+    @total_points = @total_field_goals + @total_pats
 
   end
   def bio
