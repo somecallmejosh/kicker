01088746da82671744a9c20ada463fb9318ab874
somecallmejosh <josh@thebrileys.com>
2013-12-11 09:34:31 -0500

    Add prev and next highlights buttons.

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,3 +1,11 @@
 class Highlight < ActiveRecord::Base
   attr_accessible :comments, :date, :link, :opponent, :title
+
+  def previous_highlight
+    self.class.first(:conditions => ["title < ?", title], :order => "title desc")
+  end
+
+  def next_highlight
+    self.class.first(:conditions => ["title > ?", title], :order => "title asc")
+  end
 end
