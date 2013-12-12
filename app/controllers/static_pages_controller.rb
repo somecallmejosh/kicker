3f62c14e667f7b90b5adc878e54d197fa32b4dc6
somecallmejosh <josh@thebrileys.com>
2013-12-11 20:47:26 -0500

    Calculate averages and percentages for kick socring.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,32 @@
 class StaticPagesController < ApplicationController
   def home
-  	@field_goals = FieldGoal.all
-  	@total_field_goals = FieldGoal.count(:conditions => { :is_good => true }) * 3
-  	@field_goal_long = FieldGoal.maximum('distance', :conditions => {:is_good => true})
+    @field_goals = FieldGoal.all
+    @total_field_goals = FieldGoal.count(:conditions => { :is_good => true }) * 3
+    @field_goal_long = FieldGoal.maximum('distance', :conditions => {:is_good => true})
 
-  	@point_after_attempts = PointAfterAttempt.all
-  	@total_pats = PointAfterAttempt.count(:conditions => { :is_good => true })
+    @field_goal_count = FieldGoal.count('is_good')
+    @field_goals_made = FieldGoal.count(:conditions => { :is_good => true })
+    @field_goal_percent = (@field_goals_made.to_f / @field_goal_count.to_f * 100).round(1)
 
-  	@kickoffs = Kickoff.all
+
+    @point_after_attempts = PointAfterAttempt.all
+    @total_pats = PointAfterAttempt.count(:conditions => { :is_good => true })
+
+    @kickoffs = Kickoff.all
     @total_kickoff_yards = Kickoff.sum('distance')
     @kickoff_long = Kickoff.maximum('distance')
+    @total_kicks = Kickoff.count('distance')
+    @touchback_totals = Kickoff.count(:conditions => "distance > 60")
+    @touchback_percentage = (@touchback_totals.to_f / @total_kicks.to_f * 100).round(1)
+    @average_hangtime = Kickoff.average('hangtime').round(1)
+
 
     @punts = Punt.all
     @punt_long = Punt.maximum('distance')
 
-    @total_points = @total_field_goals + @total_pats
-
+    @total_points = Integer(@total_field_goals.to_f + @total_pats.to_f)
   end
   def bio
+
   end
 end
