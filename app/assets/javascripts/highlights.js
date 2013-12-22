2a8031e572995d8957398d86ffc1ec32a25f12a9
somecallmejosh <josh@thebrileys.com>
2013-12-22 11:47:06 -0500

    Move active class from li to anchor for highlight filter.

--- app/assets/javascripts/highlights.js
+++ app/assets/javascripts/highlights.js
@@ -1,11 +1,11 @@
 $(function() {
-    $('.kick-type-select li').on('click', function(e) {
+    $('.kick-type-select li a').on('click', function(e) {
         e.preventDefault();
         var clicked = $.trim( $(this).attr("class").toLowerCase() );
         if ( clicked == 'show-all-kicks' ) {
-            $(".highlights").find('.kickoff, .fieldgoal, .punt').show();
+            $(".highlights").find('.kickoff, .fieldgoal, .punt, .pat').show();
         }else{
-            $(".highlights").find('.kickoff, .fieldgoal, .punt').hide();
+            $(".highlights").find('.kickoff, .fieldgoal, .punt, .pat').hide();
             $(".highlights").find('.' + clicked).show();
         }
 
