070cc247b278bac79f7a9fb97cc71218937cb1bd
somecallmejosh <josh@thebrileys.com>
2013-12-25 22:51:33 -0500

    Update production mailer info.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ Kicker::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "thebrileys.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["josh@thebrileys.com"],
+    password: ENV["hrookie"]
+  }
 end
