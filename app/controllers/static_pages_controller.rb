930693393b70f4259262e2125f92e1b77f9d2cde
somecallmejosh <josh@thebrileys.com>
2013-12-28 21:51:09 -0500

    Reorganize admin view.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,19 @@
 class StaticPagesController < ApplicationController
   def admin
+  	@awards = Award.order(:date)
+  	@college_wish_lists = CollegeWishList.all
+  	@field_goals = FieldGoal.order(:date)
+  	@college_wish_lists = CollegeWishList.all
+  	@highlights = Highlight.order('id ASC')
+  	@kickoffs = Kickoff.order(:date)
+  	@point_after_attempts = PointAfterAttempt.order(:date)
+  	@punts = Punt.order(:date)
+  	@references = Reference.all
+  	@bios = Bio.all
+  	respond_to do |format|
+  	  format.html # index.html.erb
+  	  format.json { render json: @awards }
+  	end
 
   end
   def landing
